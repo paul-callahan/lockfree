4a432ba0f690e7f386b23aea8ad351e66df49962
Paul Callahan <paul.callahan@gmail.com>
2014-07-08 21:07:03 -0700

    add file

---
+++ NonBlockingReadMap.hpp
@@ -0,0 +1,63 @@
+//
+//  NonBlockingReadMap.h
+//  agent-php
+//
+//  Created by Paul Callahan on 7/8/14.
+//  Copyright (c) 2014 Paul Callahan. All rights reserved.
+//
+
+#ifndef __agent_php__NonBlockingReadMap__
+#define __agent_php__NonBlockingReadMap__
+
+#include <iostream>
+
+#include <unordered_map>
+#include <atomic>
+#include <pthread.h>
+
+typedef std::unordered_map<std::string, std::string> StringMap;
+
+
+class NonBlockingReadMap {
+private:
+    pthread_mutex_t fMutex;
+    std::atomic<StringMap*> fReadMapReference;
+    
+    std::atomic<std::shared_ptr<StringMap>> fspReadMapReference;
+    
+public:
+    
+    NonBlockingReadMap() {
+        fReadMapReference.store(new StringMap());
+    }
+    
+    ~NonBlockingReadMap() {
+        //so, nothing here?
+    }
+    
+    std::string get(std::string &key) {
+        return fReadMapReference.load()->at(key);
+    }
+    
+    void put(std::string &key, std::string &value) {
+        pthread_mutex_lock(&fMutex);
+        std::shared_ptr<StringMap> psCurrentReadMap =  fspReadMapReference.load();
+        std::shared_ptr<StringMap> spMapCopy(new StringMap(*psCurrentReadMap));
+        std::pair<std::string, std::string> kvPair(key, value);
+        spMapCopy->insert(kvPair);
+        fReadMapReference.store(spMapCopy.get());
+        pthread_mutex_unlock(&fMutex);
+    }
+    
+    void clear() {
+        pthread_mutex_lock(&fMutex);
+        std::shared_ptr<StringMap> psCurrentReadMap =  fspReadMapReference.load();
+        std::shared_ptr<StringMap> spMapCopy(new StringMap(*psCurrentReadMap));
+        fReadMapReference.store(spMapCopy.get());
+        psCurrentReadMap->clear();
+        pthread_mutex_unlock(&fMutex);
+    }
+    
+};
+
+#endif /* defined(__agent_php__NonBlockingReadMap__) */
